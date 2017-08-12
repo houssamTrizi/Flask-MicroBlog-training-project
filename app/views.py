c778073681385e5aa17a4937a82cd4302379dcb1
houssam trizi <houssam.trizi@gmail.com>
2017-08-12 14:54:08 +0100

    add login view

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template(
+        'login.html',
+        title='Sign In',
+        form = form
+    )
 
 @app.route('/')
 @app.route('/index')
